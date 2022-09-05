bc014485ff6115d89eb2626f9a6db89a266660b3
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2022-09-05 21:32:09 +0100

    idk

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { existsSync, readFileSync, unlinkSync, writeFileSync } from "node:fs";
 import { join } from "path";
 
 export class SilentAPI {
+    /** Get a piece of data. */
     get(id: string, callback: Function) {
         id = id.split("/")[1];
         id += ".json";
@@ -10,12 +11,14 @@ export class SilentAPI {
         callback(data.toString());
     }
 
+    /** Make a piece of data. */
     post(data: object, id: string, callback: Function) {
         const path = ["./data", id, ".json"].join("");
         writeFileSync(path, JSON.stringify(data), 'utf-8');
         callback();
     }
     
+    /** Modify an already existing piece of data. */
     patch(data: object, id: string, callback: Function) {
         const path = ["./data", id, ".json"].join("");
         if(existsSync(path)) {
@@ -24,8 +27,9 @@ export class SilentAPI {
         callback();
     }
 
+    /** Delete a piece of data. */
     delete(id: string, callback: Function) {
-        const path = ["./data", id, ".json"].join();
+        const path = ["./data", id, ".json"].join("");
         unlinkSync(path);
         callback();
     }
--- src/test.ts
+++ src/test.ts
@@ -10,6 +10,18 @@ api.get("/testFile", (data: string) => {
     console.log(data);
 }) 
 
-api.get("/filethatdoesntexist", (data: string) => {
-    console.log("oh dear!")
+api.patch({ message: "wow, it's different now!"}, "/testFile", () => {
+    console.log("It changed!");
+})
+
+api.get("/testFile", (data: string) => {
+    console.log(data);
+}) 
+
+api.delete("/testFile", () => {
+    console.log("It's gone now...")
+}) 
+
+api.post({bestNumber: 1748}, "/folder/nested", () => {
+    console.log("We need to go deeper!")
 })
